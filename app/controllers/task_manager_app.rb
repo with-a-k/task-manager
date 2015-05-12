5cdbd76690198f4abec6f6e98dd73906f3fa1a30
Erik Butcher <with_a_k@mac.com>
2015-05-12 11:33:43 -0600

    Whoops I did the whole thing before even initing

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,27 @@
+class TaskManagerApp < Sinatra::Base
+  require 'models/task_manager'
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = TaskManager.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    TaskManager.create(params[:task])
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :show
+  end
+end
